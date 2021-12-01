3c639fa5c23b8e511d346139f3caf9cd1c7de5ea
ArielCastilloE <arielcastillo.es@gmail.com>
2021-12-01 11:43:10 -0500

    Actualización a 01-12-2021 11:43

--- app/src/main/java/com/service/viajemos/gestion/InformacionViajemos.java
+++ app/src/main/java/com/service/viajemos/gestion/InformacionViajemos.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.service.viajemos.R;
+import com.service.viajemos.login.LoginActivity;
 
 public class InformacionViajemos extends AppCompatActivity {
 
@@ -53,17 +56,40 @@ public class InformacionViajemos extends AppCompatActivity {
              consultarPerfil();
         }
 
+        crearViaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(perfilBD.equals(CONDUCTOR)){
+                    Intent intent = new Intent(InformacionViajemos.this, CrearServicio.class);
+                    intent.putExtra("correo", correoBD);
+                    intent.putExtra("id", idUserBD);
+                    intent.putExtra("perfil",perfilBD);
+                    startActivity(intent);
+                }
+                else if(perfilBD.equals(PASAJERO)){
+                   Toast.makeText(InformacionViajemos.this,"Tu perfil es de Pasajero y no puedes crear servicios",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
     }
 
     private void consultarPerfil(){
-        mDatabase.child(idUserBD).addValueEventListener(new ValueEventListener() {
+        mDatabase.child("Usuarios").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
-                    perfilBD = snapshot.child("perfil").getValue().toString();
-                    Toast.makeText(InformacionViajemos.this,"Perfil Recuperado: ", Toast.LENGTH_LONG).show();
+                    perfilBD = snapshot.child(idUserBD).child("Perfil").getValue().toString();
+                    Toast.makeText(InformacionViajemos.this,"Perfil Recuperado: " +perfilBD, Toast.LENGTH_LONG).show();
 
                 }
+                else {
+                    Toast.makeText(InformacionViajemos.this,"Error Consulta  "+ idUserBD, Toast.LENGTH_LONG).show();
+                }
+
             }
 
             @Override
